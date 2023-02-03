5a3b5b3df3b677c5fb9fa4e7e30a71d1121ee1e6
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-03 21:23:22 +0530

    Started adding Javascript

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// CODE EXPLAINED channel
+
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST, id;
+
+// get item from localstorage
+let data = localStorage.getItem("TODO");
+
+// check if data is not empty
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+}else{
+    // if data isn't empty
+    LIST = [];
+    id = 0;
+}
